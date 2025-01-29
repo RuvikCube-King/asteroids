1c3e09c611232c2c97bd2272748755b33b1f1e62
RuvikCube-King <bradley.james.polk@gmail.com>
2025-01-28 20:02:06 -0500

    added forward and backward movement as well as groups

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main ():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     print("Starting asteroids!")
@@ -17,9 +20,12 @@ def main ():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for thing in updateable:
+            thing.update(dt)
+
         screen.fill(000)
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60)/1000)
 
